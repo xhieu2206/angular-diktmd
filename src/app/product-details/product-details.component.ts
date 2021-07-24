e48032547fb424febaa15188b3daf1ff832c50e4
xhieu2206 <xhieu94@gmail.com>
2021-07-24 21:42:04 +0700

    Finish In-app navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product | undefined;
+  constructor(
+    private route: ActivatedRoute // contains những information về route và route's parameters. Bằng cách inject `ActivatedRoute`, chúng ta có thể configurring component sử dụng service
+  ) { }
 
   ngOnInit() {
+    // đầu tiên lấy ID của prodict từ route
+    const routeParams = this.route.snapshot.paramMap;
+    const id = Number(routeParams.get('id'));
+    console.log(`CLOG ngOnInit "id": `, id);
+    this.product = products.find(product => product.id === id);
   }
 
 }
