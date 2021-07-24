040adb52944f2ca27b6064c1e0d3869723e14e27
xhieu2206 <xhieu94@gmail.com>
2021-07-24 22:31:54 +0700

    Finish Managing Data

---
+++ src/app/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  addToCart(product: Product): void {
+    this.items.push(product);
+  }
+
+  getItems(): Product[] {
+    return this.items;
+  }
+
+  clearCart(): Product[] {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
+  }
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,15 +11,22 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   constructor(
-    private route: ActivatedRoute // contains những information về route và route's parameters. Bằng cách inject `ActivatedRoute`, chúng ta có thể configurring component sử dụng service
+    private route: ActivatedRoute, // contains những information về route và
+    // route's parameters. Bằng cách inject `ActivatedRoute`, chúng ta có thể
+    // component sử dụng service
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
     // đầu tiên lấy ID của prodict từ route
     const routeParams = this.route.snapshot.paramMap;
     const id = Number(routeParams.get('id'));
-    console.log(`CLOG ngOnInit "id": `, id);
+
     this.product = products.find(product => product.id === id);
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('your product has been added to the card!');
+  }
 }
